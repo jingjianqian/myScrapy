2ff9dc2a4c5b37903632b7184091e266e216dc06
joker <18697998680@163.com>
2023-02-25 14:48:35 +0800

    通过cookie登录开始

--- workDaily/workDaily/spiders/workingHours.py
+++ workDaily/workDaily/spiders/workingHours.py
@@ -1,3 +1,5 @@
+import re
+
 import scrapy
 
 from workDaily.items import Captcha_codeItem
@@ -6,36 +8,42 @@ from workDaily.items import Captcha_codeItem
 class WorkinghoursSpider(scrapy.Spider):
     name = "workingHours"
     allowed_domains = ["ame.primeton.com"]
-    start_urls = [
-        "https://ame.primeton.com/default/common/jsp/codeImage.jsp?name=verifyCode&imageHeight=21&length=4&type=number&timestamp=0.294451878200674"]
+    start_urls = ["https://ame.primeton.com/", "https://ame.primeton.com/default/ame/clipview/com.primeton.eos.ame_common.wx_worktime.wx_wtime.biz.ext?year=2023&month=2"]
     login_url = "https://ame.primeton.com/default/org.gocom.abframe.auth.LoginManager.verifyCode.biz.ext"
+    cookies = {
+        "JSESSIONID": "4B217DA6BFEA1AF0D294B966541CEF9F",
+        "cas-login": "\"CASTGC=TGT-34738-Bg3NGqnXmEafZEvoWvC4A7x5gTAMCtgc2Plg4vdtPapbBhbQlw\"",
+        "jingjq": "%2Fdefault%2Fame%2Flogin%2Fimage%2Fyuanbao1.jpg"
+    }
 
     def parse(self, response, *args, **kwargs):
-        print(2222222)
-        print(response.text)
-        pass
-        # yield scrapy.Request(
-        #     self.start_urls[0],
-        #     callback=self.parse_login
-        # )
+        main_page_list = re.findall(r'href = "(.*?)"', response.text)
+        print(main_page_list)
+        cookies = "JSESSIONID=4B217DA6BFEA1AF0D294B966541CEF9F; cas-login=\"CASTGC=TGT-34738-Bg3NGqnXmEafZEvoWvC4A7x5gTAMCtgc2Plg4vdtPapbBhbQlw\"; jingjq=%2Fdefault%2Fame%2Flogin%2Fimage%2Fyuanbao1.jpg"
+        if len(main_page_list) > 0:
+            yield scrapy.Request(
+                self.start_urls[1],
+                cookies=self.cookies,
+                dont_filter=True,
+                callback=self.parse_login
+            )
+        else:
+            print("cookie失效！！")
 
     # 解析登录页面,得到验证码链接
     def parse_login(self, response):
         # item = Captcha_codeItem
-        url = response.url  #.xpath('/html/body/img/@src').extract()
-        yield scrapy.Request(
-            url,
-            callback=self.login
-        )
+        print("ahahha")
+        print(response.body)
+        # url = response.url  #.xpath('/html/body/img/@src').extract()
+        # yield scrapy.Request(
+        #     url,
+        #     callback=self.login
+        # )
 
     def login(self, response):
-        print("2222222")
-        print("2222222")
-        print("2222222")
-        print("2222222")
-        print("2222222")
-        print("2222222")
-        print("2222222")
-        print("2222222")
-        print(response.text)
-        print(11)
+        yield scrapy.Request(
+            self.start_urls[0],
+            cookies=self.cookies,
+            callback=self.parse_login
+        )
